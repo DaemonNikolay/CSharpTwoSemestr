989a9ce44e7827190a204e7030049a14a8129e06
DaemonNikolay <nikolay-eckert@mail.ru>
2018-03-20 11:42:53 +0700

    Create class for querys and template for library API

---
+++ ZeroCDN_Client/ZeroCDN_Client/WebClientEx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZeroCDN_Client
+{
+    public class WebClientEx : WebClient
+    {
+        private CookieContainer _cookieContainer = new CookieContainer();
+
+        protected override WebRequest GetWebRequest(Uri address)
+        {
+            WebRequest request = base.GetWebRequest(address);
+            if (request is HttpWebRequest)
+            {
+                (request as HttpWebRequest).CookieContainer = _cookieContainer;
+            }
+
+            return request;
+        }
+    }
+}
